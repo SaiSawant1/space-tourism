97cb9fff556ad871e44c5d36f1fd7fce46cef843
Sai Sawant <saisawant2003@gmail.com>
2023-06-13 01:53:21 +0530

    added animation for the toggle menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = ({}) => {
   return (
     <nav className='h-10 tablet:h-24 w-full relative flex justify-between items-center '>
         <Image src={`/shared/logo.svg`} alt='logo' width={40} height={40} className='object-contain cursor-pointer'/>
-        <div className='tablet:flex hidden bg-black h-full w-2/3'>
+        <div className='tablet:flex hidden  bg-black h-full w-2/3'>
           <ul className='justify-between items-center   flex text-p-white h-full w-full px-11'>
             <div className='relative'><Link href='/'><li className='barlow font-thin'>HOME</li></Link><span className='absolute h-1 -bottom-10 bg-white w-full '></span></div>
             <div><Link href='/destination'><li className='barlow'>DESTINATION</li><span></span></Link></div>
--- src/components/ToggleMenu.tsx
+++ src/components/ToggleMenu.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const ToggleMenu = () => {
   return (
-    <div className='fixed tablet:hidden toggle_menu top-0 px-8 bottom-0 right-0 left-1/3 text-p-white'>
+    <div className='fixed tablet:hidden toggle_menu menu top-0 px-8 bottom-0 right-0 left-1/3 text-p-white'>
         <ul className='my-32 text-sm '>
             <Link href='/'>
             <li className='my-6 barlow  cursor-pointer'>
