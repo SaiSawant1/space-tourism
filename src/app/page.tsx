4b713d9ffeb500acfea181d80b2b4531d6c03d89
Sai Sawant <saisawant2003@gmail.com>
2023-06-14 02:14:15 +0530

    home page final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ContentHome, Explore } from '@/components'
 
 export default function Home() {
   return (
-    <main className='flex flex-col mt-12 desktop:flex-row desktop:justify-between desktop:px-40 desktop:items-center  tablet:mt-2 tablet:justify-evenly justify-evenly grow  '>
+    <main className='flex flex-col mt-12 desktop:flex-row desktop:justify-evenly desktop:items-center default:m-0 tablet:mt-20 tablet:justify-evenly justify-evenly grow  '>
       <ContentHome/>
       <Explore/>
     </main>
