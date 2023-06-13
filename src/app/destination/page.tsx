8c623f45d4efec8ec831562219cc8de97639bc24
Sai Sawant <saisawant2003@gmail.com>
2023-06-14 04:12:22 +0530

    Made changes to layout, fixed bug while going from main home screen to destination cleaned up the styles

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,7 +1,12 @@
-export default function Destination() {
-    return (
-      <main className="page">
-        page
+import {  Navbar } from "@/components";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen -z-10 flex flex-col p-6 bg-no-repeat bg-cover bg-background-des-m layout_des-tablet layout_des-desktop ">
+      <Navbar />
+      <main className="flex flex-col mt-12 desktop:flex-row desktop:justify-evenly desktop:items-center default:m-0 tablet:mt-20 tablet:justify-evenly justify-evenly grow  ">
+        
       </main>
-    )
-  }
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { ContentHome, Explore } from '@/components'
-
+import { ContentHome, Explore, Navbar } from "@/components";
 
 export default function Home() {
   return (
-    <main className='flex flex-col mt-12 desktop:flex-row desktop:justify-evenly desktop:items-center default:m-0 tablet:mt-20 tablet:justify-evenly justify-evenly grow  '>
-      <ContentHome/>
-      <Explore/>
-    </main>
-  )
+    <div className="w-full h-screen  flex flex-col p-6 bg-no-repeat bg-cover bg-background-home-m layout_home-tablet layout_home-desktop">
+      <Navbar />
+      <main className="flex flex-col mt-12 desktop:flex-row desktop:justify-evenly desktop:items-center default:m-0 tablet:mt-20 tablet:justify-evenly justify-evenly grow  ">
+        <ContentHome />
+        <Explore />
+      </main>
+    </div>
+  );
 }
