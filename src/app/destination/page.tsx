fd21caddd8ad172579a6363e387215b9227536c9
Sai Sawant <saisawant2003@gmail.com>
2023-06-15 01:18:44 +0530

    screen bug fixed

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -3,7 +3,7 @@ import DesPage from "@/components/destination/DesPage";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen  flex flex-col p-6 bg-no-repeat bg-cover bg-background-des-m layout_des-tablet layout_des-desktop ">
+    <div className="w-full h-full  flex flex-col p-6 bg-no-repeat bg-cover bg-background-des-m layout_des-tablet layout_des-desktop ">
       <Navbar />
       <main className="flex flex-col mt-6 desktop:flex-row desktop:justify-evenly desktop:items-center default:m-0 tablet:mt-6 tablet:justify-evenly grow  ">
         <DesPage/>
