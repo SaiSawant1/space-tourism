1867655edc10fca03ea3d10180110e3675bc9a77
Sai Sawant <saisawant2003@gmail.com>
2023-06-20 01:29:38 +0530

    skeleton loading added to page destination

--- src/components/ImageSkeleton.tsx
+++ src/components/ImageSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ImageSkeleton = () => {
   return (
-    <div className='object-contain bg-white animate-pulse max-w-[170px] max-h-[170px] tablet:min-h-[400px]  tablet:min-w-[400px] desktop:min-w-[400px] desktop:min-h-[400px]'>
+    <div className='object-contain bg-white animate-pulse max-w-[170px] max-h-[170px] tablet:min-h-[400px] rounded-[50%] tablet:min-w-[400px] desktop:min-w-[400px] desktop:min-h-[400px]'>
 
     </div>
   )
