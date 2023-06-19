05220d65c5fdf71f7c12cad36b89575c06640245
Sai Sawant <saisawant2003@gmail.com>
2023-06-20 01:12:08 +0530

    skelecton loading -v1.0

---
+++ src/components/ImageSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageSkeleton = () => {
+  return (
+    <div className='object-contain bg-white animate-pulse max-w-[170px] max-h-[170px] tablet:min-h-[400px]  tablet:min-w-[400px] desktop:min-w-[400px] desktop:min-h-[400px]'>
+
+    </div>
+  )
+}
+
+export default ImageSkeleton
--- src/components/destination/MoonImage.tsx
+++ src/components/destination/MoonImage.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import { FC } from "react";
+import { Suspense } from "react";
+import ImageSkeleton from "../ImageSkeleton";
 const MoonImage = ({ title }: { title: string }) => {
   return (
     <div className="flex desktop:self-center content__center my-7 w-full desktop:h-full ">
-      <Image
-        src={`/destination/image-${title.toLowerCase()}.png`}
-        className=" object-contain w-[170px] h-[170px] tablet:h-[400px]  tablet:w-[400px] desktop:w-[400px] desktop:h-[400px] "
-        alt="journey"
-        width={170}
-        height={170}
-      />
+      <Suspense fallback={<ImageSkeleton/>}>
+        <Image
+          src={`/destination/image-${title.toLowerCase()}.png`}
+          className=" object-contain w-[170px] h-[170px] tablet:h-[400px]  tablet:w-[400px] desktop:w-[400px] desktop:h-[400px] "
+          alt="journey"
+          width={170}
+          height={170}
+        />
+      </Suspense>
     </div>
   );
 };
